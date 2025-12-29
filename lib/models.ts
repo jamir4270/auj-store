ada32148fd1dd8d017c7a4cc9897febfe9f82287
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-12-29 17:20:29 +0800

    Created data models for database tables

---
+++ lib/models.ts
@@ -0,0 +1,59 @@
+export type ProductCategory =
+  | "school_supplies"
+  | "snacks"
+  | "beverages"
+  | "accessories"
+  | "others";
+export type OrderStatus = "complete" | "incomplete";
+export type PrintServiceType = "photocopy" | "print";
+export type PrintPaperType = "copier" | "photo";
+export type PrintPaperSizeType =
+  | "short"
+  | "a4"
+  | "long"
+  | "2r"
+  | "3r"
+  | "4r"
+  | "5r";
+export type PrintColorMode = "b&w" | "color";
+
+export interface Product {
+  id: string;
+  name: string;
+  quantity: number;
+  price: number;
+  category: ProductCategory;
+  created_at: string;
+}
+
+export interface Order {
+  id: string;
+  total: number;
+  status: OrderStatus;
+  partial_payment: number;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface OrderItem {
+  id: string;
+  order_id: string;
+  product_id: string;
+  quantity: number;
+  unit_price_at_sale: number;
+  subtotal: number;
+  created_at: string;
+}
+
+export interface print_job {
+  id: string;
+  order_id: string;
+  service_type: PrintServiceType;
+  paper_type: PrintPaperType;
+  paper_size: PrintPaperSizeType;
+  color_mode: PrintColorMode;
+  page_count: number;
+  copies: number;
+  subtotal: number;
+  created_at: string;
+}
