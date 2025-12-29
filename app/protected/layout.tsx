9999a172ae46346ee53a8751682b894d0aa54218
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-12-29 21:56:34 +0800

    added header title

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -58,8 +58,10 @@ export default async function ProtectedLayout({
     <SidebarProvider defaultOpen={defaultOpen}>
       <AppSidebar routes={routes} />
       <SidebarInset>
-        <header className="flex h-14 shrink-0 items-center gap-2 border-b px-4">
+        <header className="flex flex-row justify-between w-full h-14 shrink-0 items-center border-b px-4">
           <SidebarTrigger />
+          <div className="font-bold">AUJ Store Management</div>
+          <div></div>
         </header>
 
         <main className="flex flex-1 flex-col gap-4 p-4">{children}</main>
