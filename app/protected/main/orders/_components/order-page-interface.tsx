679d81a5fba209b6bb6fd461c2a6c82c90ff7b2f
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2026-01-01 22:04:04 +0800

    added adding items and deleting items

--- app/protected/main/orders/_components/order-page-interface.tsx
+++ app/protected/main/orders/_components/order-page-interface.tsx
@@ -12,15 +12,22 @@ import {
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Product } from "@/lib/models";
+import { ProductAtSale } from "@/lib/models";
 import { twoDecimal } from "@/lib/utils";
+import { Minus, MinusCircle, PlusIcon, Trash, Trash2 } from "lucide-react";
 
 type OrderProps = {
-  products: Product[];
+  products: ProductAtSale[];
 };
 
 export function OrderInterface({ products }: OrderProps) {
   const [isOrdering, setIsOrdering] = useState(false);
+  const [orderList, setOrderList] = useState<ProductAtSale[]>([]);
+  console.log(`Parent isOrdering: ${isOrdering}`);
+
+  const handleOrderListUpdate = (newList: ProductAtSale[]) => {
+    setOrderList(newList);
+  };
   return (
     <div className="flex flex-row h-full gap-3 px-3">
       <div className="flex flex-col flex-2 w-full gap-5">
@@ -28,8 +35,11 @@ export function OrderInterface({ products }: OrderProps) {
           <div className="flex flex-row justify-between">
             <CardTitle className="text-3xl">Orders</CardTitle>{" "}
             <Button
-              onClick={() => setIsOrdering(!isOrdering)}
-              disabled={isOrdering ? true : false}
+              onClick={() => {
+                setIsOrdering(true);
+                console.log(`Parent isOrdering: ${isOrdering}`);
+              }}
+              disabled={isOrdering}
             >
               Order
             </Button>
@@ -40,7 +50,15 @@ export function OrderInterface({ products }: OrderProps) {
           <CardHeader className="flex flex-col gap-5">
             <Input type="search" placeholder="Search products..."></Input>
             {products?.map((product) => {
-              return <ProductCard key={product.id} product={product} />;
+              return (
+                <ProductCard
+                  key={product.id}
+                  product={product}
+                  productList={orderList}
+                  updateIsOrdering={setIsOrdering}
+                  updateProductList={handleOrderListUpdate}
+                />
+              );
             })}
           </CardHeader>
           <CardContent></CardContent>
@@ -51,12 +69,31 @@ export function OrderInterface({ products }: OrderProps) {
         <div className="flex flex-col flex-1 w-full animate-in fade-in slide-in-from-right-5 duration-300">
           <Card>
             <CardHeader>
-              <CardTitle></CardTitle>
-              <CardDescription></CardDescription>
+              <CardTitle>Order Details</CardTitle>
+              <CardDescription>List of orders</CardDescription>
+              {orderList.map((order) => {
+                return (
+                  <OrderItemCard
+                    key={order.id}
+                    product={order}
+                    orderList={orderList}
+                    updateProductList={setOrderList}
+                  />
+                );
+              })}
             </CardHeader>
             <CardContent></CardContent>
             <CardFooter>
-              <Button onClick={() => setIsOrdering(!isOrdering)}>Cancel</Button>
+              <Button
+                onClick={() => {
+                  setIsOrdering(false);
+                  console.log(`Parent isOrdering: ${isOrdering}`);
+                  const emptyArray: ProductAtSale[] = [];
+                  setOrderList(emptyArray);
+                }}
+              >
+                Cancel
+              </Button>
             </CardFooter>
           </Card>
         </div>
@@ -66,7 +103,10 @@ export function OrderInterface({ products }: OrderProps) {
 }
 
 type ProductProps = {
-  product: Product;
+  product: ProductAtSale;
+  productList: ProductAtSale[];
+  updateIsOrdering: (isOrdering: boolean) => void;
+  updateProductList: (newList: ProductAtSale[]) => void;
 };
 
 function setStatusColor(status: string) {
@@ -79,10 +119,41 @@ function setStatusColor(status: string) {
   }
 }
 
-function ProductCard({ product }: ProductProps) {
+function ProductCard({
+  product,
+  productList = [],
+  updateIsOrdering,
+  updateProductList,
+}: ProductProps) {
+  const [isClicked, setIsClicked] = useState(false);
+
+  function handleOnClick() {
+    if (product.status !== "out_of_stock") {
+      updateIsOrdering(true);
+      if (productList.includes(product)) {
+        updateProductList(productList);
+      } else {
+        const newList: ProductAtSale[] = [...productList, product];
+        updateProductList(newList);
+        setIsClicked(true);
+      }
+    } else {
+      updateProductList(productList);
+    }
+  }
   return (
     <div>
-      <Card className="p-0">
+      <Card
+        className={`p-0 ${
+          product.status === "out_of_stock" && "border-red-500"
+        } ${
+          isClicked &&
+          product.status !== "out_of_stock" &&
+          productList.includes(product) &&
+          "border-green-400"
+        }`}
+        onClick={handleOnClick}
+      >
         <CardHeader className="flex flex-row justify-between items-center">
           <div className="flex flex-col h-full gap-1">
             <CardTitle className="mt-2">{product.name}</CardTitle>
@@ -92,6 +163,84 @@ function ProductCard({ product }: ProductProps) {
         </CardHeader>
         <CardFooter className={setStatusColor(product.status ?? "")}>
           {product.status}
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
+type OrderItemCardProps = {
+  product: ProductAtSale;
+  orderList: ProductAtSale[];
+  updateProductList: (product: ProductAtSale[]) => void;
+};
+
+function OrderItemCard({
+  product,
+  orderList,
+  updateProductList,
+}: OrderItemCardProps) {
+  const [amount, setAmount] = useState(1);
+
+  function handleDelete() {
+    const newProductList = orderList.filter((item) => {
+      return item.id !== product.id;
+    });
+    updateProductList(newProductList);
+  }
+
+  return (
+    <div>
+      <Card className={`p-0`}>
+        <CardHeader className="flex flex-row justify-between items-center">
+          <div className="flex flex-col h-full gap-1">
+            <CardTitle className="mt-2">{product.name}</CardTitle>
+            <CardDescription>{product.category}</CardDescription>
+          </div>
+          <div className="text-2xl">
+            {twoDecimal(product.subtotal as number)}
+          </div>
+        </CardHeader>
+        <CardFooter className="flex flex-row justify-between">
+          <div className="flex flex-row gap-1">
+            <Minus
+              onClick={() => {
+                if (amount >= 1 && amount <= product.quantity) {
+                  setAmount(amount - 1);
+                  product.amount = amount;
+                } else {
+                  setAmount(1);
+                  product.amount = amount;
+                }
+              }}
+            />
+            <Input
+              type="number"
+              min={1}
+              max={product.quantity}
+              value={amount}
+              onChange={(event) =>
+                setAmount(
+                  parseInt(!event.target.value ? "1" : event.target.value)
+                )
+              }
+              className="w-20 text-center"
+            ></Input>
+            <PlusIcon
+              onClick={() => {
+                if (amount >= 1 && amount <= product.quantity) {
+                  setAmount(amount + 1);
+                  product.amount = amount;
+                } else {
+                  setAmount(1);
+                  product.amount = amount;
+                }
+              }}
+            />
+          </div>
+          <div>
+            <Trash2 className="text-red-500" onClick={handleDelete} />
+          </div>
         </CardFooter>
       </Card>
     </div>
--- app/protected/main/orders/page.tsx
+++ app/protected/main/orders/page.tsx
@@ -1,8 +1,9 @@
 import { fetchProducts } from "@/lib/data";
 import { OrderInterface } from "./_components/order-page-interface";
+import { ProductAtSale } from "@/lib/models";
 
 export default async function Orders() {
-  const products = await fetchProducts();
+  const products: ProductAtSale[] = await fetchProducts();
   return (
     <div>
       <OrderInterface products={products} />
