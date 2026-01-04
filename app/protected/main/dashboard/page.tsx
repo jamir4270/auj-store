a5c260ecb53c4b0efdf87efa4326d947cb6628d4
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2026-01-04 17:30:27 +0800

    added summary cards

--- app/protected/main/dashboard/page.tsx
+++ app/protected/main/dashboard/page.tsx
@@ -1,3 +1,32 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Dashboard() {
-  return <div>This is Dashboard page.</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-row justify-between">
+        <div>Date Here</div>
+        <div>Time Here</div>
+      </div>
+      <div className="flex flex-row justify-between w-full gap-5">
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Gross Sales</CardTitle>
+          </CardHeader>
+          <CardContent>2000</CardContent>
+        </Card>
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Net Profit</CardTitle>
+          </CardHeader>
+          <CardContent>1000.00</CardContent>
+        </Card>
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Products Sold</CardTitle>
+          </CardHeader>
+          <CardContent>100</CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
