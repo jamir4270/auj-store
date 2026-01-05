a4bb1ef0eb153895f0f4f7dad28c1aa6ab8d5c51
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2026-01-05 16:25:22 +0800

    added toast to log in page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import NextTopLoader from "nextjs-toploader";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -35,6 +36,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <NextTopLoader />
+          <Toaster position="top-right" />
           {children}
         </ThemeProvider>
       </body>
