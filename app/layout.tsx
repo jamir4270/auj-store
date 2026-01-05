9fe0225d59d694de46d0d65afdfd90cc04e94c65
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2026-01-05 16:39:05 +0800

    updated website metadata and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "AUJ Store",
+  description: "Manage sari-sari store seemleasly and efficiently.",
 };
 
 const geistSans = Geist({
